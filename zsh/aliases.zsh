59469080953061e8da19729051d7ad796fc1e234
pokepoke81 <4258646+pokepoke81@users.noreply.github.com>
2025-11-18 10:25:11 -0500

    Include direnv hook in aliases.zsh
    Add direnv hook to zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,8 @@ alias reload='source ~/.zshrc'
 
 # Reset the mouse after a remote tmux disconnect
 alias resetmouse='printf '"'"'\e[?1000l'"'"
+
+# direnv hook
+if command -v direnv &> /dev/null; then
+  eval "$(direnv hook zsh)"
+fi
